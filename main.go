ddcf5c71401f9c21d84564e51a66876d0e9d564f
Dmytro Andreichykov <dmytro.andreichykov@hyperjar.com>
2025-09-15 17:42:34 +0300

    added main function

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, _ := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("Response Body:", string(body))
+}
