27aee7b1e0e56ed474a278b089347f972443c2ed
Dmytro Andreichykov <dmytro.andreichykov@hyperjar.com>
2025-09-15 17:56:08 +0300

    initial server code

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	requestPath := r.URL.Path // Get the path from the request URL
+	fmt.Printf("Received request for path: %s\n", requestPath)
+	io.WriteString(w, fmt.Sprintf("You requested the path: %s\n", requestPath))
+}
+
 func main() {
-	resp, _ := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("Response:", string(body))
+	http.HandleFunc("/", handler) // Register the handler for all paths
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServe(":8080", nil) // Start the server
 }
